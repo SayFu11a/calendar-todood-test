598f5befac4729768dc23309711810cbbf665625
SayFu11a <sgishlakaev@mail.ru>
2024-07-09 17:41:51 +0300

    fix data bag

--- src/components/Day.tsx
+++ src/components/Day.tsx
@@ -14,7 +14,7 @@ const Day: React.FC<DayProps> = ({ day, month, year }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isHoliday, setIsHoliday] = useState(false);
   const { tasks } = useTasks();
-  const date = new Date(year, month, day).toISOString().split('T')[0];
+  const date = new Date(year, month, day).toLocaleDateString('en-CA'); // Используем toLocaleDateString для корректного отображения даты
 
   useEffect(() => {
     const hd = new Holidays('US'); // Вы можете указать код страны по своему желанию
